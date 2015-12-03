a5916c4849a595cfd5c119f73443fdc75c2c55ca
Hank Kuo <hank_kuo@hiiir.com>
2015-12-03 11:09:33 +0800

    refectory test.webpack file position & using require

--- karma.conf.js
+++ karma.conf.js
@@ -38,46 +38,18 @@ module.exports = function(config) {
 
     files: [
       //'./node_modules/phantomjs-polyfill/bind-polyfill.js',
-      './webpack/test.config.js'
+      './webpack/test.preprocessors.js'
     ],
 
     preprocessors: {
-      './webpack/test.config.js': ['webpack', 'sourcemap']
+      './webpack/test.preprocessors.js': ['webpack', 'sourcemap']
     },
 
     reporters: ['mocha'],
 
     plugins: plugins,
 
-    webpack: {
-      devtool: 'inline-source-map',
-      module: {
-        loaders: [
-          {test: /\.js$/, exclude: /node_modules/, loaders: ['babel']},
-          {test: /\.json$/, loader: 'json-loader' },
-          {test: /\.css$/, loader: 'style-loader!css-loader'},
-          {test: /\.scss$/, loader: 'style!css?modules&importLoaders=2&sourceMap&localIdentName=[local]___[hash:base64:5]!autoprefixer?browsers=last 2 version!sass?outputStyle=expanded&sourceMap'},
-          {test: /\.(jpe?g|png|gif|svg)$/, loader: 'url', query: {limit: 8192}}
-        ]
-      },
-      resolve: {
-        modulesDirectories: [
-          'src',
-          'node_modules'
-        ],
-        extensions: ['', '.json', '.js']
-      },
-      plugins: [
-        new webpack.IgnorePlugin(/\.json$/),
-        new webpack.NoErrorsPlugin(),
-        new webpack.DefinePlugin({
-          __CLIENT__: true,
-          __SERVER__: false,
-          __DEVELOPMENT__: true,
-          __DEVTOOLS__: false  // <-------- DISABLE redux-devtools HERE
-        })
-      ]
-    },
+    webpack: require('./webpack/test.config'),
     webpackServer: {
       noInfo: true
     }
--- webpack/test.config.js
+++ webpack/test.config.js
@@ -1,2 +1,32 @@
-var context = require.context('../src', true, /-test\.js$/);
-context.keys().forEach(context);
+require('babel-core/polyfill');
+var webpack = require('webpack');
+
+module.exports = {
+  devtool: 'inline-source-map',
+  module: {
+    loaders: [
+      {test: /\.js$/, exclude: /node_modules/, loaders: ['babel']},
+      {test: /\.json$/, loader: 'json-loader' },
+      {test: /\.css$/, loader: 'style-loader!css-loader'},
+      {test: /\.scss$/, loader: 'style!css?modules&importLoaders=2&sourceMap&localIdentName=[local]___[hash:base64:5]!autoprefixer?browsers=last 2 version!sass?outputStyle=expanded&sourceMap'},
+      {test: /\.(jpe?g|png|gif|svg)$/, loader: 'url', query: {limit: 8192}}
+    ]
+  },
+  resolve: {
+    modulesDirectories: [
+      'src',
+      'node_modules'
+    ],
+    extensions: ['', '.json', '.js']
+  },
+  plugins: [
+    new webpack.IgnorePlugin(/\.json$/),
+    new webpack.NoErrorsPlugin(),
+    new webpack.DefinePlugin({
+      __CLIENT__: true,
+      __SERVER__: false,
+      __DEVELOPMENT__: true,
+      __DEVTOOLS__: false  // <-------- DISABLE redux-devtools HERE
+    })
+  ]
+};
